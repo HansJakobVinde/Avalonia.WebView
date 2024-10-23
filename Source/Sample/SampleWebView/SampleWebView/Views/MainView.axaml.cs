e9fd3406cdf67f85fa9b4ac8afafab3d65311a81
Hans Jakob Vinde <03hjvinde@gmail.com>
2024-10-23 16:35:29 +0200

    Multiple WebViews in sample

--- Source/Sample/SampleWebView/SampleWebView/Views/MainView.axaml.cs
+++ Source/Sample/SampleWebView/SampleWebView/Views/MainView.axaml.cs
@@ -7,10 +7,20 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
         //PART_Button.Click += PART_Button_Click;
-        PART_WebView.WebViewNewWindowRequested += PART_WebView_WebViewNewWindowRequested;
+        PART_WebView1.WebViewNewWindowRequested += PART_WebView1_WebViewNewWindowRequested;
+        PART_WebView2.WebViewNewWindowRequested += PART_WebView2_WebViewNewWindowRequested;
+        PART_WebView3.WebViewNewWindowRequested += PART_WebView3_WebViewNewWindowRequested;
     }
 
-    private void PART_WebView_WebViewNewWindowRequested(object? sender, WebViewCore.Events.WebViewNewWindowEventArgs e)
+    private void PART_WebView1_WebViewNewWindowRequested(object? sender, WebViewCore.Events.WebViewNewWindowEventArgs e)
+    {
+        e.UrlLoadingStrategy = WebViewCore.Enums.UrlRequestStrategy.OpenInWebView;
+    }
+    private void PART_WebView2_WebViewNewWindowRequested(object? sender, WebViewCore.Events.WebViewNewWindowEventArgs e)
+    {
+        e.UrlLoadingStrategy = WebViewCore.Enums.UrlRequestStrategy.OpenInWebView;
+    }
+    private void PART_WebView3_WebViewNewWindowRequested(object? sender, WebViewCore.Events.WebViewNewWindowEventArgs e)
     {
         e.UrlLoadingStrategy = WebViewCore.Enums.UrlRequestStrategy.OpenInWebView;
     }
